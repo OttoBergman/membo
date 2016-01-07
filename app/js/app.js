188413f140d4804f67305bc1dd6291456356e1a9
oerlex <alex.sp@posteo.de>
2016-01-07 10:01:34 +0100

    pushed all my changes from the last weeks

--- app/js/app.js
+++ app/js/app.js
@@ -34,3 +34,21 @@ memboApp.config(['$routeProvider',
         redirectTo: '/home'
       });
   }]);
+
+
+angular.module('datepickerBasicUsage',
+    ['ngMaterial', 'ngMessages']).controller('AppCtrl', function($scope) {
+  $scope.myDate = new Date();
+  $scope.minDate = new Date(
+      $scope.myDate.getFullYear(),
+      $scope.myDate.getMonth() - 2,
+      $scope.myDate.getDate());
+  $scope.maxDate = new Date(
+      $scope.myDate.getFullYear(),
+      $scope.myDate.getMonth() + 2,
+      $scope.myDate.getDate());
+  $scope.onlyWeekendsPredicate = function(date) {
+    var day = date.getDay();
+    return day === 0 || day === 6;
+  }
+});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -43,6 +43,8 @@ memboControllers.controller('EventsCtrl', ['$scope', '$routeParams', 'ngDialog',
           })
     };
 
+
+
     function DialogController($scope, $mdDialog) {
       $scope.member = {};
       $scope.tempMember = {};
@@ -55,30 +57,22 @@ memboControllers.controller('EventsCtrl', ['$scope', '$routeParams', 'ngDialog',
       $scope.cancel = function() {
         $mdDialog.cancel();
       };
-      $scope.answer = function(answer) {
-        $mdDialog.hide(answer);
+      $scope.answer = function() {
+        $mdDialog.hide();
       }
       $scope.ifEmpty = function(){
 
       }
+        $scope.opendatepicker = function(){
+            $('.datepicker').pickadate({
+                selectMonths: true, // Creates a dropdown to control month
+                selectYears: 15 // Creates a dropdown of 15 years to control year
+            });
+        }
     }
 
-    angular.module('datepickerBasicUsage',
-        ['ngMaterial', 'ngMessages']).controller('AppCtrl', function($scope) {
-      $scope.myDate = new Date();
-      $scope.minDate = new Date(
-          $scope.myDate.getFullYear(),
-          $scope.myDate.getMonth() - 2,
-          $scope.myDate.getDate());
-      $scope.maxDate = new Date(
-          $scope.myDate.getFullYear(),
-          $scope.myDate.getMonth() + 2,
-          $scope.myDate.getDate());
-      $scope.onlyWeekendsPredicate = function(date) {
-        var day = date.getDay();
-        return day === 0 || day === 6;
-      }
-    });
+
+
 
   }]);
 
